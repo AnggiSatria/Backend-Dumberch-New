842f916a0846227be5a248342dbc6a5d0c967347
AnggiSatria <anggi.satria@talentinsider.com>
2024-07-01 01:08:03 +0700

    harus fix

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,19 +1,36 @@
 const Sequelize = require("sequelize");
-const db = {};
-const sequelize = new Sequelize("integration-b32", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: console.log,
-  freezeTableName: true,
+require("dotenv").config(); // Load environment variables from .env file
 
+// Initialize Sequelize with DATABASE_URL from environment variables
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: console.log, // Log all SQL queries to console
+  freezeTableName: true, // Prevent Sequelize from pluralizing table names
   pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+    max: 5, // Maximum number of connection in pool
+    min: 0, // Minimum number of connection in pool
+    acquire: 30000, // Maximum time, in milliseconds, that pool will try to get connection before throwing error
+    idle: 10000, // Maximum time, in milliseconds, that a connection can be idle before being released
   },
 });
 
+// Test the database connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+const db = {};
 db.sequelize = sequelize;
 
 module.exports = db;
